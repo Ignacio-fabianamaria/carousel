0e7e6463ac51e4eceabcc1a849f849a14999051f
Siigmaf <faby.tbe@gmail.com>
2022-11-27 11:17:35 -0300

    implement carousel

--- carousel-app/src/App.js
+++ carousel-app/src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 // import Buttons from './components/Buttons';
-import left from './images/left.png';
-import rigth from './images/right.png';
 import Carousel from './components/Carousel';
 import sale from './images/sale.png';
 
@@ -9,18 +7,10 @@ function App() {
   return (
     <div className="container">
       <img src={sale} alt='sale' className='sale'/>
-      <div className='carousel'>
+      <div className='carousel-app'>
       <Carousel />
-      </div>
-      <div className='buttons'>
-      <button type='button' className='left'>
-        <img src={left} alt='left' className='left' />
-      </button>
-      <button type='button' className='rigth'>
-        <img src={rigth} alt='rigth' className='rigth' />
-      </button>
-      </div>
-    </div>
+     </div>
+     </div>
   );
 }
 
--- carousel-app/src/components/Carousel.jsx
+++ carousel-app/src/components/Carousel.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import left from '../images/left.png';
+import rigth from '../images/right.png';
 
-export default function Carousel (){
-const [data, setData] = useState([]);
+export default function Carousel() {
+    const [data, setData] = useState([]);
+    const carousel = useRef(null);
 
-async function requestAPI() {
-    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=cel';
-    const response = await fetch(endpoint);
-    const result = await response.json();
+    async function requestAPI() {
+        const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=cel';
+        const response = await fetch(endpoint);
+        const result = await response.json();
         setData(result.results);
         console.log(result.results);
     }
@@ -15,20 +18,46 @@ async function requestAPI() {
         requestAPI();
     }, []);
 
-    return(
-        <div className='carousel'>
+    const leftClick = (e) => {
+       // e.preventDefault();
+        console.log(carousel.current.scrollLeft);
+        carousel.current.scrollLeft -= carousel.current.offsetWidth
+    }
 
-        {
-            data?.map(({id, thumbnail, title, price}) =>(
-                <div key={id} className='card-cel'>
-                    <img src={thumbnail} alt={title} className='img-card'/>
-                    <div className='info'>
-                    <p className='name-card'> 20% OFF</p>
-                    <span>R$:{price}</span>
-                    </div>
-                </div>
-            ))
-        }
+    const rigthClick = (e) => {
+        e.preventDefault();
+        console.log(carousel.current.scrollRigth);
+        carousel.current.scrollLeft += carousel.current.offsetWidth
+    }
+
+    return (
+        <div>
+            <div className='carousel' ref={carousel}>
+
+                {
+                    data?.map(({ id, thumbnail, title, price }) => (
+                        <div key={id} className='card-cel'>
+                            <img src={thumbnail} alt={title} className='img-card' />
+                            <div className='info'>
+                                <p className='name-card'> 20% OFF</p>
+                                <span>R$:{price}</span>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+            <div className='buttons'>
+                <button type='button'
+                    className='left'
+                    onClick={leftClick}>
+                    <img src={left} alt='left' className='left' />
+                </button>
+                <button type='button'
+                    className='rigth'
+                    onClick={rigthClick}>
+                    <img src={rigth} alt='rigth' className='rigth' />
+                </button>
+            </div>
         </div>
     )
 }
